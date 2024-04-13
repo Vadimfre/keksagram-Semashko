7e2083139419d9e37f16c705f9640ce5a89cb04a
Vadim Semashko <vsemashko250@gmail.com>
2024-04-13 07:50:20 +0300

    еще раз попробовал поправить нейминг констант

--- kekstagram-clear/main.js
+++ kekstagram-clear/main.js
@@ -1,11 +1,11 @@
 "use strict";
-const LIKE_MIN = 15;
-const LIKE_MAX = 200;
-const PHOTO_QUANTITY = 25;
-const COM_MAX = 5;
-const MAX_AVATAR_NUM = 6;
-const WIDTH_COMMENT = 35;
-const HEIGHT_COMMENT = 35;
+const MINIMUM_LIKES_COUNT = 15;
+const MAXIMUM_LIKES_COUNT = 200;
+const TOTAL_NUMBER_OF_PHOTOS = 25;
+const MAXIMUM_COMMENTS_COUNT = 5;
+const MAXIMUM_AVATAR_INDEX = 6;
+const AVATAR_IMAGE_WIDTH = 35;
+const AVATAR_IMAGE_HEIGHT = 35;
 
 const COMMENTS = [
   `Всё отлично!`,
@@ -43,10 +43,10 @@ createPhotosData();
 uploadPhotos();
 
 function createPhotosData() {
-  for (let i = 1; i <= PHOTO_QUANTITY; i++) {
+  for (let i = 1; i <= TOTAL_NUMBER_OF_PHOTOS; i++) {
     const photoData = {
       url: `photos/${i}.jpg`,
-      likes: getRandomInt(LIKE_MIN, LIKE_MAX),
+      likes: getRandomInt(MINIMUM_LIKES_COUNT, MAXIMUM_LIKES_COUNT),
       comments: [],
       description: getRandomArrayElement(DESCRIPTIONS),
     };
@@ -57,12 +57,12 @@ function createPhotosData() {
 }
 
 function createComments() {
-  const commentsCount = getRandomInt(1, COM_MAX);
+  const commentsCount = getRandomInt(1, MAXIMUM_COMMENTS_COUNT);
   const commentsData = [];
 
   for (let j = 1; j <= commentsCount; j++) {
     const commentData = {
-      avatar: `img/avatar-${getRandomInt(1, MAX_AVATAR_NUM)}.svg`,
+      avatar: `img/avatar-${getRandomInt(1, MAXIMUM_AVATAR_INDEX)}.svg`,
       message: getRandomArrayElement(COMMENTS),
       name: getRandomArrayElement(NAMES),
     };
@@ -73,7 +73,7 @@ function createComments() {
 
 function uploadPhotos() {
   const pictureTemplate = document.querySelector("#picture").content;
-  for (let photo of photosData) {
+  for (const photo of photosData) {
     const pictureElement = pictureTemplate.cloneNode(true);
 
     pictureElement.querySelector(".picture__img").src = photo.url;
@@ -108,8 +108,8 @@ function getCommentImg(elem) {
   commentPhoto.classList.add("social__picture");
   commentPhoto.src = elem.avatar;
   commentPhoto.alt = `Аватар комментатора фотографии`;
-  commentPhoto.width = WIDTH_COMMENT;
-  commentPhoto.height = HEIGHT_COMMENT;
+  commentPhoto.width = AVATAR_IMAGE_WIDTH;
+  commentPhoto.height = AVATAR_IMAGE_HEIGHT;
   return commentPhoto;
 }
 
@@ -122,7 +122,7 @@ function getCommentParagraph(elem) {
 }
 
 function createFullSizePictureComment() {
-  for (let elem of firstPhoto.comments) {
+  for (const elem of firstPhoto.comments) {
     const commentElement = document.createElement("li");
 
     commentElement.classList.add("social__comment");
